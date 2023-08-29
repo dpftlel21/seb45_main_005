316d358f3c77de40ca9a3f1ee0d081ffb10bf425
dpftlel21 <dkdnmju@gmail.com>
2023-08-29 10:39:06 +0900

    TS, storybook 환경설정 최종

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-      <div>
-        Hello Typescript
-      </div>
-  );
-}
+  return <div>Hello Typescript</div>;
+};
 
 export default App;
