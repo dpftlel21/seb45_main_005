4312ccb6d4b1a05717d7fd6be11d2651bf51cdaa
jsue98 <idtodrkr9893@gmail.com>
2023-08-29 17:06:04 +0900

    [style] DB 설정 추가
    mysql 연결 설정

--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/entity/Member.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/entity/Member.java
@@ -1,4 +1,20 @@
 package mainproject.musicforecast.domain.member.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column
+    private String email;
 }
