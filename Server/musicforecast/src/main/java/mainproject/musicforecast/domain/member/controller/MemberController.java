32907345ba55d187078f0964ff7db52b2c7c5563
jsue98 <idtodrkr9893@gmail.com>
2023-08-31 01:02:28 +0900

    :art: [style]: 예외처리 추가

--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/controller/MemberController.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/controller/MemberController.java
@@ -3,27 +3,54 @@ package mainproject.musicforecast.domain.member.controller;
 import mainproject.musicforecast.domain.member.dto.MemberPatchDto;
 import mainproject.musicforecast.domain.member.dto.MemberPostDto;
 import mainproject.musicforecast.domain.member.entity.Member;
+import mainproject.musicforecast.domain.member.mapper.MemberMapper;
+import mainproject.musicforecast.domain.member.service.MemberService;
+import mainproject.musicforecast.global.exception.response.ErrorResponse;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/members")
+@Validated
 public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+    public MemberController(MemberService memberService, MemberMapper mapper) {
+        this.memberService = memberService;
+        this.mapper = mapper;
+    }
 
     //회원가입
     @PostMapping ("/signup")
-    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
+        Member member = mapper.memberPostDtoToMember(memberPostDto);
+
+        Member response = memberService.createMember(member);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
     //회원정보 수정
     @PatchMapping("/profile/edit/{memberId}")
     public ResponseEntity patchMember(@PathVariable("memberId") @Positive long memberId,
-                                      @RequestBody MemberPatchDto memberPatchDto) {
-        return new ResponseEntity<>(HttpStatus.OK);
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto) {
+
+        memberPatchDto.setMemberId(memberId);
+
+        Member member = mapper.memberPatchDtoToMember(memberPatchDto);
+
+        Member response = memberService.updateMember(member);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
     //회원 자기소개글 목록 조회 기능
     @GetMapping("/profile/{memberId}")
--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/service/MemberService.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/service/MemberService.java
@@ -1,15 +1,32 @@
 package mainproject.musicforecast.domain.member.service;
 
+import mainproject.musicforecast.domain.member.dto.MemberResponseDto;
 import mainproject.musicforecast.domain.member.entity.Member;
+import mainproject.musicforecast.domain.member.repository.MemberRepository;
+import mainproject.musicforecast.global.exception.BusinessLogicException;
+import mainproject.musicforecast.global.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     public Member createMember(Member member) {
+
+        memberRepository.save(member);
+
         return member;
     }
 
     public Member updateMember(Member member) {
+
+        Member findMember = findVerifiedMember(member.getMemberId());
+
         return member;
     }
 
@@ -31,5 +48,19 @@ public class MemberService {
     public void deleteMember(long memberId) {
         // 지금은 void
         // 탈퇴 회원 정보를 삭제하지 않고 상태만 변경할 경우 return 타입 변경 필요
+    }
+
+    @Transactional(readOnly = true)
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+//멤버 상태가 있을 경우 사용
+//        if(findMember.getMemberStatus().equals(Member.MemberStatus.MEMBER_QUIT))
+//            throw new BusinessLogicException(ExceptionCode.MEMBER_STATUS_DELETE);
+
+        return findMember;
     }
 }
