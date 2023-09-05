9321977faa99a8de88720c482cdd519c5a43a94c
occcasion <occcasion.gmail.com>
2023-09-05 12:49:32 +0900

    feat: weather
    날씨별 장르에 따른 플레이리스트 추천

--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/playlist/repository/PlaylistRepository.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/playlist/repository/PlaylistRepository.java
@@ -1,12 +1,13 @@
 package mainproject.musicforecast.domain.playlist.repository;
 
-import mainproject.musicforecast.domain.member.entity.Member;
 import mainproject.musicforecast.domain.playlist.entity.Playlist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
