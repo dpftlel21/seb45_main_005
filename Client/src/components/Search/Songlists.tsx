048901bb25e17629f5db900c3a6b570b44b8b20e
dpftlel21 <dkdnmju@gmail.com>
2023-09-09 22:52:52 +0900

    ✨ feat: 노래 검색 및 클릭시 체크박스 변화

--- Client/src/components/Search/Songlists.tsx
+++ Client/src/components/Search/Songlists.tsx
@@ -1,8 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../redux/store';
+import { selectedSonglist } from '../../redux/slice/SonglistsSlice';
 
 const SongLists = () => {
+  const [selectedItems, setSelectedItems] = useState([]); // 선택된 항목을 저장할 상태
+
   const SongData = useSelector((state: RootState) => state.songlists.value);
+
+  const dispatch = useDispatch();
+
+  const handleSongClick = (el: object) => {
+    const isSelected = selectedItems.includes(el);
+    if (isSelected) {
+      setSelectedItems(selectedItems.filter((item) => item !== el));
+    } else {
+      setSelectedItems([...selectedItems, el]);
+      dispatch(selectedSonglist(el));
+    }
+  };
 
   return (
     <>
@@ -19,8 +35,18 @@ const SongLists = () => {
       {/* 플리 노래목록 */}
       <ul className="w-full h-[500px] flex flex-col overflow-y-scroll">
         {SongData.map((el, index) => (
-          <li className="w-full h-[100px] grid grid-cols-6 items-center text-center border-b-2 border-solid border-gray-400 border-opacity-20 hover:bg-[#a3b2f5de] ">
-            <input type="checkbox" />
+          <li
+            className={`w-full h-[100px] grid grid-cols-6 items-center text-center border-b-2 border-solid border-gray-400 border-opacity-20 ${
+              selectedItems.includes(el) ? 'bg-blue-200' : ''
+            }`}
+            key={index}
+            onClick={() => handleSongClick(el)} // li 요소 클릭 시 토글 함수 호출
+          >
+            <input
+              type="checkbox"
+              onClick={() => handleSongClick(el)}
+              checked={selectedItems.includes(el)}
+            />
             <h3>{index + 1}</h3>
             <div className="flex justify-center items-center text-center">
               <img src={el.imageUrl} className="w-[50px] h-[50px] mr-4" />
