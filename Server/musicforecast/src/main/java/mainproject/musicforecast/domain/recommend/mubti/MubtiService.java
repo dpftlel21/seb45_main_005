c52ac1ca46d1df95b19b80e9d4427168d8d97cc0
occcasion <occcasion.gmail.com>
2023-09-13 12:54:21 +0900

    fix: tag
    mubti/날씨별 tag 분류

--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/recommend/mubti/MubtiService.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/recommend/mubti/MubtiService.java
@@ -35,52 +35,85 @@ public class MubtiService {
         if (a == 1) {
             switch (b) {
                 case 1:
-                    genre.add("신나는");
+                    genre.add("우울한");
+                    genre.add("차분한");
+                    genre.add("센치한");
+                    genre.add("잔잔한");
                     break;
                 case 2:
-                    genre.add("발라드");
+                    genre.add("힙한");
+                    genre.add("파워풀한");
+                    genre.add("치명적인");
                     break;
                 case 3:
                     genre.add("신나는");
+                    genre.add("상쾌한");
                     break;
                 case 4:
-                    genre.add("신나는");
+                    genre.add("몽글몽글한");
+                    genre.add("편안한");
+                    genre.add("행복한");
                     break;
                 default:
+                    genre.add("신나는");
                     break;
             }
         } else if (a == 2) {
             switch (b) {
                 case 1:
+                    genre.add("힙한");
                     genre.add("신나는");
+                    genre.add("상쾌한");
+                    genre.add("파워풀한");
+                    genre.add("웅장한");
                     break;
                 case 2:
+                    genre.add("몽글몽글한");
+                    genre.add("편안한");
+                    genre.add("행복한");
+                    genre.add("힙한");
+                    genre.add("하이틴");
                     genre.add("신나는");
+                    genre.add("가벼운");
                     break;
                 case 3:
-                    genre.add("신나는");
+                    genre.add("차분한");
+                    genre.add("편안한");
+                    genre.add("잔잔한");
+                    genre.add("센치한");
                     break;
                 case 4:
+                    genre.add("힙한");
                     genre.add("신나는");
+                    genre.add("편안한");
+                    genre.add("차분한");
                     break;
                 default:
+                    genre.add("신나는");
                     break;
             }
         } else if (a == 3) {
             switch (b) {
                 case 1:
                     genre.add("신나는");
+                    genre.add("힙한");
+                    genre.add("상쾌한");
+                    genre.add("가벼운");
                     break;
                 case 2:
-                    genre.add("신나는");
+                    genre.add("잔잔한");
+                    genre.add("차분한");
+                    genre.add("따뜻한");
+                    genre.add("감성적인");
                     break;
                 case 3:
-                    genre.add("신나는");
+                    genre.add("클래식");
                     break;
                 case 4:
                     // adminSuggest
                     break;
                 default:
+                    genre.add("신나는");
                     break;
             }
         } else {
