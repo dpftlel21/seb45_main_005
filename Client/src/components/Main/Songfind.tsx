0f732b654858d6962339d30db2a804b9715c61df
MS_btff <whdghks845@gmail.com>
2023-09-14 17:57:41 +0900

    ✨feat:노래 기능 추가

--- Client/src/components/Main/Songfind.tsx
+++ Client/src/components/Main/Songfind.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useScroll, motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 // import List from '../../assets/images/list.jpg';
 
-const Chart = () => {
+const Songfind = () => {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -15,9 +14,9 @@ const Chart = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (scrollYProgress.get() > 0.1) {
+      if (scrollYProgress.get() > 0.9) {
         setIsVisible(true);
-      } else {
+      } else if (scrollYProgress.get() < 0.9) {
         setIsVisible(false);
       }
     };
@@ -29,25 +28,28 @@ const Chart = () => {
     };
   }, [scrollYProgress]);
 
+  console.log(ref.current);
+  console.log(scrollYProgress.get());
+
   const textMotion = {
     initial: { opacity: 0 },
     animate: { opacity: isVisible ? 1 : 0, y: isVisible ? 0 : -50 },
     exit: { opacity: 0, y: isVisible ? -50 : 0 },
-    transition: { duration: 2, ease: 'easeInOut' },
+    transition: { duration: 1, ease: 'easeInOut' },
   };
 
   const imageMotion = {
     initial: { opacity: 0 },
     animate: { opacity: isVisible ? 1 : 0, y: isVisible ? 1 : -75 },
     exit: { opacity: 0, y: isVisible ? -50 : 0 },
-    transition: { duration: 2, ease: 'easeInOut', delay: 1 },
+    transition: { duration: 1, ease: 'easeInOut' },
   };
 
   return (
     <>
       <header className="bg-cover bg-center h-screen bg-[#71b8f6]-500" style={{}}>
         <div className="w-full h-full  bg-opacity-70">
-          <div className="flex flex-col justify-center items-center font-spoqa-han-san-neo font-medium ">
+          <div className="flex flex-col justify-center items-center font-spoqa-hansans font-bold ">
             <motion.div ref={ref} {...textMotion}>
               <div className="   text-5xl mt-20 ">
                 <p>당신이 어디에 있든</p>
@@ -65,7 +67,7 @@ const Chart = () => {
                 className="w-[350px] h-[300px] my-24 "
               />
               <Link to="/weatherRecommend">
-                <div className="w-[200px] h-12 bg-[#D5E5F0] font-spoqa-han-san-neo text-2xl ml-16 rounded-lg  flex items-center justify-center hover:opacity-90">
+                <div className="w-[200px] h-12 bg-[#86abe0] font-spoqa-han-san-neo text-2xl ml-16 rounded-lg  flex items-center justify-center hover:opacity-90">
                   노래 찾으러 가기
                 </div>
               </Link>
@@ -76,4 +78,4 @@ const Chart = () => {
     </>
   );
 };
-export default Chart;
+export default Songfind;
