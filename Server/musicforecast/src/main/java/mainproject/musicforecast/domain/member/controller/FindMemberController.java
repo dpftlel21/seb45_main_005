a7b2e1bef6121ba3eeb87951f6a18daf45956d73
jsue98 <idtodrkr9893@gmail.com>
2023-09-18 17:22:29 +0900

    :sparkles: [feat] : 회원정보 찾기
    - 수정 예정

--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/controller/FindMemberController.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/controller/FindMemberController.java
@@ -4,6 +4,7 @@ import mainproject.musicforecast.domain.member.dto.FindMemberDto;
 import mainproject.musicforecast.domain.member.entity.Member;
 import mainproject.musicforecast.domain.member.mapper.MemberMapper;
 import mainproject.musicforecast.domain.member.service.FindMemberService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,18 +19,29 @@ public class FindMemberController {
         this.mapper = mapper;
         this.findMemberService = findMemberService;
     }
-    @PatchMapping("/pw")
-    public ResponseEntity searchPw(@RequestBody FindMemberDto findMemberDto) {
+
+    @GetMapping("/username")
+    public ResponseEntity findId(@RequestBody FindMemberDto findMemberDto) {
 
         Member member = mapper.findMemberDto(findMemberDto);
 
-        Member response = findMemberService.searchPw(member);
+        Member response = findMemberService.findId(member);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
-    @GetMapping("/{email}")
-    public ResponseEntity findQuestion(@PathVariable("email") String email) {
-        Member member = findMemberService.findQuestion(email);
-        return new ResponseEntity<>(mapper.memberToFindMemberDto(member), HttpStatus.OK);
-    }
+//    @PatchMapping("/pw")
+//    public ResponseEntity searchPw(@RequestBody FindMemberDto findMemberDto) {
+//
+//        Member member = mapper.findMemberDto(findMemberDto);
+//
+//        Member response = findMemberService.searchPw(member);
+//
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/user")
+//    public ResponseEntity findQuestion(@PathVariable("email") String email) {
+//        Member member = findMemberService.findQuestion(email);
+//        return new ResponseEntity<>(mapper.memberToFindMemberDto(member), HttpStatus.OK);
+//    }
 }
--- Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/service/FindMemberService.java
+++ Server/musicforecast/src/main/java/mainproject/musicforecast/domain/member/service/FindMemberService.java
@@ -1,16 +1,44 @@
 package mainproject.musicforecast.domain.member.service;
 
 import mainproject.musicforecast.domain.member.entity.Member;
+import mainproject.musicforecast.domain.member.repository.MemberRepository;
+import mainproject.musicforecast.global.exception.BusinessLogicException;
+import mainproject.musicforecast.global.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FindMemberService {
-    private MemberService memberService;
-    public FindMemberService(MemberService memberService) {
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+    public FindMemberService(MemberService memberService,
+                             MemberRepository memberRepository) {
         this.memberService = memberService;
+        this.memberRepository = memberRepository;
     }
     public Member searchPw(Member user) {
     return user;
+    }
+
+    public Member findId(Member user) {
+        String nickname = user.getNickname();
+        long birthdate = user.getBirthdate();
+
+        Optional<Member> optionalMember = memberRepository.findByNicknameAndBirthdate(nickname, birthdate);
+
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        if(user.getQuestionNumber() != findMember.getQuestionNumber()) {
+            new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+        }
+        if(user.getAuth_answer() != findMember.getAuth_answer()) {
+            new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+        }
+
+        return findMember;
     }
 
     public Member findQuestion(String email) {
